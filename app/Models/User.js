5d6f2bddd22dac82dd4efe218feee9e922475d3c
Renan Machado <renan.mav@hotmail.com>
2018-12-29 20:45:30 -0300

    Relationship map between Post and User

--- app/Models/User.js
+++ app/Models/User.js
@@ -18,6 +18,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  posts () {
+    return this.hasMany('App/Models/Post')
+  }
 }
 
 module.exports = User
