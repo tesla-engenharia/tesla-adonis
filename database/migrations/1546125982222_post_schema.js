165072e4f8282d1554e00e0e6ef2d395d3883bea
Renan Machado <renan.mav@hotmail.com>
2018-12-29 20:37:34 -0300

    Post migration

---
+++ database/migrations/1546125982222_post_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', table => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id')
+        .inTable('files')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table.string('title').notNullable()
+      table.text('content').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
