0956b9e6959d48e601fdeb873714a25b85a93101
Renan Machado <renan.mav@hotmail.com>
2018-12-29 21:29:53 -0300

    CRUD of Posts

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,92 +1,73 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Post = use('App/Models/Post')
 
-/**
- * Resourceful controller for interacting with posts
- */
 class PostController {
   /**
    * Show a list of all posts.
    * GET posts
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index () {
+    const posts = await Post.query()
+      .with('user')
+      .with('file')
+      .fetch()
 
-  /**
-   * Render a form to be used for creating a new post.
-   * GET posts/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return posts
   }
 
   /**
    * Create/save a new post.
    * POST posts
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, auth }) {
+    const data = request.only(['file_id', 'title', 'content'])
+
+    const post = await Post.create({ ...data, user_id: auth.user.id })
+
+    return post
   }
 
   /**
    * Display a single post.
    * GET posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
+  async show ({ params }) {
+    const post = await Post.findOrFail(params.id)
 
-  /**
-   * Render a form to update an existing post.
-   * GET posts/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    await post.load('user')
+    await post.load('file')
+
+    return post
   }
 
   /**
    * Update post details.
    * PUT or PATCH posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request }) {
+    const post = await Post.findOrFail(params.id)
+
+    const data = request.only(['file_id', 'title', 'content', 'user_id'])
+
+    post.merge(data)
+
+    await post.save()
+
+    await post.load('user')
+    await post.load('file')
+
+    return post
   }
 
   /**
    * Delete a post with id.
    * DELETE posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params }) {
+    const post = await Post.findOrFail(params.id)
+
+    await post.delete()
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -9,4 +9,14 @@ Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
 Route.get('files/:id', 'FileController.show')
-Route.post('files', 'FileController.store')
+
+Route.get('posts', 'PostController.index')
+Route.get('posts/:id', 'PostController.show')
+
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+
+  Route.post('posts', 'PostController.store')
+  Route.put('posts/:id', 'PostController.update')
+  Route.delete('posts/:id', 'PostController.destroy')
+}).middleware(['auth'])
