f092577a283f989d906768fca4b0349ef504900f
Renan Machado <renan.mav@hotmail.com>
2019-01-03 20:33:41 -0300

    Test for User creation

--- test/functional/auth.spec.js
+++ test/functional/auth.spec.js
@@ -1,9 +1,35 @@
 'use strict'
 
-const { test, trait } = use('Test/Suite')('Auth')
+const Suite = use('Test/Suite')('Auth')
+const { test, trait } = Suite
 const User = use('App/Models/User')
 
 trait('Test/ApiClient')
+
+const { beforeEach } = Suite
+
+beforeEach(async () => {
+  const user = await User.findBy('email', 'renan.mav@hotmail.com')
+  if (user) {
+    user.delete()
+  }
+})
+
+test('it should create a user succefully', async ({ client }) => {
+  const user = {
+    name: 'Renan Machado',
+    email: 'renan.mav@hotmail.com',
+    password: 'renanmav.97',
+    password_confirmation: 'renanmav.97'
+  }
+
+  const response = await client
+    .post('/users')
+    .send(user)
+    .end()
+
+  response.assertStatus(200)
+})
 
 test('it should be able to authenticate with valid credentials', async ({
   client,
