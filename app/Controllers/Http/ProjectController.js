bf817061ea16d784e8addbd65efa2bb421efa83d
mateussr2511 <mateussr2511@gmail.com>
2019-05-13 01:19:20 -0300

    projects and jobs models

---
+++ app/Controllers/Http/ProjectController.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const Project = use('App/Models/Project')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with projects
+ */
+class ProjectController {
+  /**
+   * Show a list of all projects.
+   * GET projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const projects = await Project.query()
+      .with('user')
+      .fetch()
+    return projects
+  }
+
+  /**
+   * Create/save a new project.
+   * POST projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    const data = request.only(['title', 'description'])
+    const project = await Project.create({
+      ...data,
+      user_id: auth.user.id
+    })
+    return project
+  }
+
+  /**
+   * Display a single project.
+   * GET projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const project = await Project.findOrFail(params.id)
+    await project.load('user')
+    await project.load('jobs')
+
+    return project
+  }
+
+  /**
+   * Render a form to update an existing project.
+   * GET projects/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+
+  async update ({ params, request }) {
+    const project = await Project.findOrFail(params.id)
+    const data = request.only(['title', 'description'])
+    project.merge(data)
+    await project.save()
+    return project
+  }
+
+  /**
+   * Delete a project with id.
+   * DELETE projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params }) {
+    const project = await Project.findOrFail(params.id)
+    await project.delete()
+  }
+}
+
+module.exports = ProjectController
